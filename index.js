83a4daeed6520392588bbaf0addf04544679c4cf
Jakub Rejch <jakubrejch@gmail.com>
2020-09-25 17:56:46 +0200

    Create sun, eartch and stars. Add variable light4 Set sun and earth in position

--- index.js
+++ index.js
@@ -9,51 +9,122 @@ const camera = new THREE.PerspectiveCamera( 24, window.innerWidth/window.innerHe
  
 const colorYellow = new THREE.Color('rgb(248, 241, 94)');
 const colorBlue = new THREE.Color('rgb(219, 253, 253)');
+const colorOrange = new THREE.Color('rgb(255, 94, 0)');
+const colorGreen = new THREE.Color('rgb(69, 136, 60)');
 
- const cubeMaterial = new THREE.MeshPhysicalMaterial({
-     color: 0x36a598,
-     shinines: 80,
-     wireframeLinewidth: 10,
-     fog: true,
-     reflectivity: 1,
- });
-
-const cubeGeometry = new THREE.SphereGeometry(2, 8, 5);
-
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-
-
-const sunMaterial = new THREE.MeshNormalMaterial({
-     color: colorYellow,
- });
- const sunGeometry = new THREE.SphereGeometry(2, 32, 32);
-
- const sun = new THREE.Mesh(sunGeometry, sunMaterial);
-
-const light = new THREE.PointLight(colorYellow, 2);
+const light = new THREE.PointLight(colorYellow, 1);
 const light2 = new THREE.PointLight(colorBlue, .5);
+const light3 = new THREE.PointLight(colorOrange, 1);
+const light4 = new THREE.PointLight(colorGreen, 10);
 
-light.position.set(-40, -20, 20);
-light2.position.set(10, 20, 40);
+light.position.set(-40, -20, 40);
+light2.position.set(10, 50, 50);
+light3.position.set(10, 50, 80);
+// light4.position.set(-90, 30, 0);
 
 scene.add(light);
 scene.add(light2);
-scene.add(cube);
-scene.add(sun);
+scene.add(light3);
+scene.add(light4);
 
-camera.position.z = (15);
-cube.rotation.y = 20;
-cube.rotation.z = -20;
+camera.position.z = 15;
+
+
+
+// stars
+const vertices = [];
+
+for (let i = 0; i < 100000; i ++ ) {
+
+	const x = THREE.MathUtils.randFloatSpread(2500);
+	const y = THREE.MathUtils.randFloatSpread(2500);
+	const z = THREE.MathUtils.randFloatSpread(2500);
+
+	vertices.push( x, y, z );
+
+}
+
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+const material = new THREE.PointsMaterial({color: colorYellow});
+
+const points = new THREE.Points(geometry, material);
+
+// earth
+
+const createEarth = (r = 0.5, colorGreen) => {
+    const earthMaterial = new THREE.MeshPhysicalMaterial({
+        color: colorGreen,
+     shinines: 40,
+     wireframeLinewidth: 10,
+     fog: true,
+     reflectivity: 2,
+    });
+    const earthGeometry = new THREE.SphereGeometry(r, 32, 32);
+    return new THREE.Mesh(earthGeometry, earthMaterial);
+};
+
+// sun
+
+const createBall = (r = 1, colorOrange) => {
+    const ballMaterial = new THREE.MeshPhongMaterial({
+        shininess: 40,
+        colorOrange,
+    });
+
+    const ballGeometry = new THREE.SphereGeometry(r, 32, 32);
+    return new THREE.Mesh(ballGeometry, ballMaterial);
+};
+
+
+const newLight = (intensity = 1, colorYellow) => {
+    return new THREE.PointLight(intensity, colorYellow);
+};
+
+const sun = createBall(2, colorOrange);
+sun.position.set(-3.5, 2, -5);
+
+const earth = createEarth(1, colorGreen);
+earth.position.set(2, -1.5, 0);
+earth.rotation.y = 20;
+earth.rotation.z = -20;
 
 
 const animate = () => {
 
-requestAnimationFrame(animate);
-cube.rotation.y += 0.01;
-cube.rotation.x += 0.015;
-cube.rotation.z += 0.01;
-renderer.render(scene, camera);
-}
-animate();
+    requestAnimationFrame(animate);
+    earth.rotation.y += 0.03;
+    // cube.rotation.x += 0.015;
+    // cube.rotation.z += 0.01;
+    renderer.render(scene, camera);
+    }
+    animate();
 
+    const eartchAnimation = () => {
+        const timer = 0.001 * Date.now();
+
+        
+    }
+
+
+const handleResize = () => {
+    const {innerWidth, innerHeight} = window;
+    renderer.setSize(innerWidth, innerHeight);
+    camera.aspect = innerWidth / innerHeight;
+    camera.updateProjectionMatrix();
+}
+
+// loop
+
+const loop = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(loop);
+};
+
+loop();
+window.addEventListener('resize', handleResize);
+
+scene.add(sun, newLight);
+scene.add(points);
+scene.add(earth);
